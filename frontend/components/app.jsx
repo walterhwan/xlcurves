974c137eadc287b2e96280b9cc8280577ffdd273
Walter Wan <WalterHWan@gmail.com>
2018-07-21 19:20:13 -0700

    Comment out Switch

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,23 +3,24 @@ import {
   Route, withRouter, Link,
   NavLink, Redirect, Switch
 } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
+// import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import LandingPage from './landing_page';
-import MainPage from './main/main_page';
-import SessionLogin from './session/session_login';
-import SessionSignup from './session/session_signup';
-import SignupFormContainer from './session/signup_form_container.jsx';
+// import MainPage from './main/main_page';
+// import SessionLogin from './session/session_login';
+// import SessionSignup from './session/session_signup';
+// import SignupFormContainer from './session/signup_form_container.jsx';
 
 
 const App = () => (
   <div>
-    <Switch>
-      // <ProtectedRoute path='/main' component={MainPage} />
-      // <AuthRoute path='/login' component={SessionLogin} />
-      // <AuthRoute path='/signup' component={SessionSignup} />
-      <AuthRoute path='/' component={LandingPage} />
-    </Switch>
+    <LandingPage />
   </div>
 );
+// <Switch>
+//   <ProtectedRoute path='/main' component={MainPage} />
+//   <AuthRoute path='/login' component={SessionLogin} />
+//   <AuthRoute path='/signup' component={SessionSignup} />
+//   <AuthRoute path='/' component={LandingPage} />
+// </Switch>
 
 export default App;
