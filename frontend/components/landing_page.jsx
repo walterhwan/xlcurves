b9349b35a1ab02d4407da9df65aae9eab8cc1af0
Walter Wan <WalterHWan@gmail.com>
2018-07-21 18:30:32 -0700

    Add logo

--- frontend/components/landing_page.jsx
+++ frontend/components/landing_page.jsx
@@ -6,8 +6,8 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div className='landing_page'>
-        <a className='xlcurves-logo'></a>
-        <h1>Welcome to xlcurves</h1>
+        <a className='logo'></a>
+        <h1 className='welcome'>Welcome to xlcurves</h1>
       </div>
     );
   }
