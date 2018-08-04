474dfe49fbe52dbd8fe2062275a56bd3a76baa6e
Walter Wan <WalterHWan@gmail.com>
2018-08-03 17:37:06 -0700

    Fix landing_page

--- frontend/components/landing_page.jsx
+++ frontend/components/landing_page.jsx
@@ -40,34 +40,11 @@ class LandingPage extends React.Component {
           </a>
           <h1 className='one-liner'>One liner to describe xlCurves</h1>
         </div>
-        
-        <div className="first-section">
+
+        <div className="first-section"></div>
       </div>
     );
   }
 }
 
 export default LandingPage;
-
-// <ul className="navbar-nav mr-auto">
-//   <li className="nav-item active">
-//     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-//   </li>
-//   <li className="nav-item">
-//     <a className="nav-link" href="#">Link</a>
-//   </li>
-//   <li className="nav-item dropdown">
-//     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//       Dropdown
-//     </a>
-//     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//       <a className="dropdown-item" href="#">Action</a>
-//       <a className="dropdown-item" href="#">Another action</a>
-//       <div className="dropdown-divider"></div>
-//       <a className="dropdown-item" href="#">Something else here</a>
-//     </div>
-//   </li>
-//   <li className="nav-item">
-//     <a className="nav-link disabled" href="#">Disabled</a>
-//   </li>
-// </ul>
