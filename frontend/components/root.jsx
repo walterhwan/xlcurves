cf02f116f545b4aae05f639c7385ad9da42f04ce
Walter Wan <WalterHWan@gmail.com>
2018-08-11 13:14:51 -0700

    Add core.js and delete polyfill

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import App from './app';
-import { HashRouter } from 'react-router-dom';
+import "core-js/fn/object/assign";
+import "core-js/fn/promise";
+import React from "react";
+import { Provider } from "react-redux";
+import App from "./app";
+import { HashRouter } from "react-router-dom";
 
 const Root = ({ store }) => (
-  <Provider store={ store }>
+  <Provider store={store}>
     <HashRouter>
       <App />
     </HashRouter>
